2a40c59ac42b74aa9ce7ef147e757af471eb7782
Hussam Turjman <hussam.turjman@gmail.com>
2024-10-18 12:09:59 +0200

    ignore formating

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,14 +1,14 @@
-from home_api.db.base import Base
-from home_api.db.session import Session
-from home_api.entrypoint import entry_point
 import sys
 import os
-import pytest
 
+# fmt: off
 cwd = os.path.join(os.path.dirname(__file__))
 parent_dir = os.path.join(cwd, "..")
 sys.path.append(parent_dir)
-
+from home_api.entrypoint import entry_point
+from home_api.db.session import Session
+from home_api.db.base import Base
+# fmt: on
 
 session = Session.create(d_Base=Base)
 
