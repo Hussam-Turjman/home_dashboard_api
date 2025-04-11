fb57107fd5aa0df3f4d92dd51bfbb1e80e87800c
Hussam Turjman <hussam.turjman@gmail.com>
2025-04-11 10:16:44 +0200

    add recreate_db.sh

---
+++ recreate_db.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+FILENAME="tmp_backup.sql"
+DB_NAME="home"
+DB_USER="hussam"
+
+pg_dump $DB_NAME >$FILENAME
+dropdb $DB_NAME
+createdb $DB_NAME
+psql --username=$DB_USER DB_NAME <$FILENAME
